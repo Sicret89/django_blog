f1974832eeada9c5726cf965bcfe275aa7997755
Kamil Banas <kamil.banas787@gmail.com>
2021-11-18 06:53:08 +0000

    first migration and db relations added

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,32 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'KamilB',
-        'title': 'Blog Post 1',
-        'content': 'First post text',
-        'date_posted': 'November 17, 2021'
-    },
-    {
-        'author': 'JackC',
-        'title': 'Blog Post 2',
-        'content': 'Second post text',
-        'date_posted': 'November 17, 2021'
-    },
-    {
-        'author': 'LeonB',
-        'title': 'Blog Post 3',
-        'content': 'Third post text',
-        'date_posted': 'November 17, 2021'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
-
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
