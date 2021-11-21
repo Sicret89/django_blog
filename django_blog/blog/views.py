da73ece748b9f70050d3eceffccbe930312f4015
Kamil Banas <kamil.banas787@gmail.com>
2021-11-21 17:08:45 +0000

    meassahe that post has beed created!

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -18,7 +18,6 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    # paginate_by = 2
 
 class PostDetailView(DetailView):
     model = Post
