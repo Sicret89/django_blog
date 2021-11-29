bc79c48ec6b3ce1835c6d173b69ebd0ae778d19e
Kamil Banas <kamil.banas787@gmail.com>
2021-11-29 19:20:35 +0000

    django heroku

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +130,8 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_S3_REGION_NAME = "eu-west-2"
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
